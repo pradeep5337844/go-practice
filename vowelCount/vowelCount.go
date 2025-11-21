1dac6622752a8f4550c6a3a88a3dce81b7d983b6
pradeep5337844 <pradeep5337844@gmail.com>
2025-11-22 00:22:00 +0530

    vowel count

---
+++ vowelCount/vowelCount.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	vowelsList := []string{"a", "e", "i", "o", "u", "A", "E", "I", "O", "U"}
+	count := 0
+	stringToCheck := "HEllo World"
+
+	for _, str := range stringToCheck {
+		for _, vowel := range vowelsList {
+			if string(str) == vowel {
+				count++
+			}
+		}
+	}
+	fmt.Println("the number of vowels in the string", stringToCheck, "is ", count)
+}
