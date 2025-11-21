6a0e9d8c050a86cc10f917538c0b2e9936a0f1d6
pradeep5337844 <pradeep5337844@gmail.com>
2025-11-21 23:38:33 +0530

    added fibonacci sequence and sum

---
+++ fibonacciSequence/fibonacciSequence.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func fibonacci(n int) []int {
+	fib := make([]int, n)
+	fib[0], fib[1] = 0, 1
+	for i := 2; i < n; i++ {
+		fib[i] = fib[i-1] + fib[i-2]
+	}
+	return fib
+}
+func main() {
+	fmt.Println(fibonacci(10))
+}
